6542cea3cea580e8f9c0276cfcbeb876d7ab40a0
Juan Rios <juan.rios.28@gmail.com>
2020-05-28 03:51:55 -0400

    module fix and readme

--- angular/ng-trivia/src/app/app.module.ts
+++ angular/ng-trivia/src/app/app.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategorySelectComponent } from './category-select/category-select.component';
 import { DecodePipe } from './decode.pipe';
 import { Option } from './option/option.component';
 import { QuestionComponent } from './question/question.component';
-import { ScoreTrackerComponent } from './score-tracker/score-tracker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategorySelectComponent,
-    ScoreTrackerComponent,
     QuestionComponent,
     Option,
     DecodePipe
